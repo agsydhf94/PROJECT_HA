92ca7ff473137cc81584e5b377d5b957c98eac1e
DESKTOP-0JKOLL7\ECLASS <agsydhf94@gmail.com>
2024-06-17 16:54:46 +0900

    InteractionUI Scripts

---
+++ Assets/Project HA/Scripts/Interaction System/InteractionUI_ListItem.cs
@@ -0,0 +1,42 @@
+using HA;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+namespace HA
+{
+    public class InteractionUI_ListItem : MonoBehaviour
+    {
+
+        public string DataKey
+        {
+            set => key = value;
+            get => key;
+        }
+
+        public string Message
+        {
+            set => text.text = value;
+        }
+
+        public bool IsSelected
+        {
+            set => selection.SetActive(value);
+        }
+
+        public IInteractable InteractionData
+        {
+            set => interactionData = value;
+            get => interactionData;
+        }
+
+
+
+        public GameObject selection;
+        public TMPro.TextMeshProUGUI text;
+
+        private IInteractable interactionData;
+        private string key;
+    }
+}
