a456015df0414ca88b9d03642a69b4de472110c8
DESKTOP-0JKOLL7\ECLASS <agsydhf94@gmail.com>
2024-07-22 16:32:43 +0900

    EnemyWaypoints initialize

--- Assets/Project HA/Scripts/Enemy/PatrolBehavior.cs
+++ Assets/Project HA/Scripts/Enemy/PatrolBehavior.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PatrolBehavior : StateMachineBehaviour
 {
     float timer = 0;
+    List<Transform> wayPoints = new List<Transform>();
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         timer = 0;
+        Transform wayPointsObject = GameObject.FindGameObjectWithTag("EnemyWaypoints").transform;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
