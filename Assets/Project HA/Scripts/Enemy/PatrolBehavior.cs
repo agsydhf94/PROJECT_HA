9fb678afbb84712a04173196fd38ee2b57ba87b4
DESKTOP-0JKOLL7\ECLASS <agsydhf94@gmail.com>
2024-07-22 17:55:27 +0900

    Detect_Overlapping Sphere

--- Assets/Project HA/Scripts/Enemy/PatrolBehavior.cs
+++ Assets/Project HA/Scripts/Enemy/PatrolBehavior.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PatrolBehavior : StateMachineBehaviour
 {
     float timer = 0;
     List<Transform> wayPoints = new List<Transform>();
+    NavMeshAgent navMeshAgent;
 
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         timer = 0;
         Transform wayPointsObject = GameObject.FindGameObjectWithTag("EnemyWaypoints").transform;
+        foreach (Transform wayPointElements in wayPointsObject)
+        {
+            wayPoints.Add(wayPointElements);
+        }
+        navMeshAgent = animator.GetComponent<NavMeshAgent>();
+        navMeshAgent.SetDestination(wayPoints[0].position);
     }
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         timer += Time.deltaTime;
---
+++ Assets/Project HA/Scripts/SplashLogic/SplashLogic.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashLogic : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        for(int i = 0; i < detectedObjects.Count; i++)
+        {
+            Gizmos.DrawLine(transform.position, detectedObjects[i].transform.position);
+        }
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    public List<GameObject> detectedObjects = new List<GameObject>();
+    public float radius = 3f;
+
+    [ContextMenu("Detect")]
+    public void DetectObjectsBySphere()
+    {
+        detectedObjects.Clear();
+        Collider[] overlappedObjects = Physics.OverlapSphere(transform.position, radius);
+        for(int i = 0; i < overlappedObjects.Length; i++)
+        {
+            Vector3 dir = transform.position - overlappedObjects[i].transform.position;
+            Ray ray = new Ray(transform.position, dir.normalized);
+            if(Physics.Raycast(ray, out RaycastHit hit, radius))
+            {
+                if(hit.transform == overlappedObjects[i].transform)
+                {
+                    detectedObjects.Add(overlappedObjects[i].gameObject);
+                }
+            }
+
+            
+        }
+    }
+}
