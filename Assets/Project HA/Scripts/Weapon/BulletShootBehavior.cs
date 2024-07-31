65da160738b1219ff7330514c46db89326a15559
DESKTOP-0JKOLL7\ECLASS <agsydhf94@gmail.com>
2024-07-31 18:17:04 +0900

    Ragdoll System

--- Assets/Project HA/Scripts/Weapon/BulletShootBehavior.cs
+++ Assets/Project HA/Scripts/Weapon/BulletShootBehavior.cs
@@ -12,34 +12,6 @@ public class BulletShootBehavior : IWeaponBehavior
 
     public void Attack(WeaponBase weapon)
     {
-        if (Time.time > lastShootTime + fireRate)
-        {
-
-            // ÃÑ¾Ë »ý¼ºÇÏ±â
-            var newBullet = Instantiate(bulletPrefab);
-            var cameraTransform = Camera.main.transform;
-            newBullet.transform.SetPositionAndRotation(cameraTransform.position, cameraTransform.rotation);
-            newBullet.gameObject.SetActive(true);
-
-
-            // ½´ÆÃ °¡´É
-            lastShootTime = Time.time;
-            gunController.PlaySE(fire_Sound);
-            crosshair.FireAnimation();
-            currentBulletCount--;
-
-            // Muzzle ÀÌÆåÆ® ¸¸µé±â
-            var newMuzzle = Instantiate(muzzlePrefab);
-            newMuzzle.transform.SetPositionAndRotation(firePosition.position, firePosition.rotation * Quaternion.Euler(90, 0, 90));
-            newMuzzle.gameObject.SetActive(true);
-            Destroy(newMuzzle, 0.12f);
-
-            
-
-            // ÅºÇÇ »ý¼º
-
-            
-
-        }
+        
     }
 }
--- Assets/Project HA/Scripts/Weapon/Gun.cs
+++ Assets/Project HA/Scripts/Weapon/Gun.cs
@@ -15,41 +15,7 @@ namespace HA
 
         public override void Shoot()
         {
-            if (Time.time > lastShootTime + fireRate)
-            {
-                // ½´ÆÃ °¡´É
-                lastShootTime = Time.time;
-                gunController.PlaySE(fire_Sound);
-                crosshair.FireAnimation();
-                currentBulletCount--;
-
-                // Muzzle ÀÌÆåÆ® ¸¸µé±â
-                var newMuzzle = Instantiate(muzzlePrefab);
-                newMuzzle.transform.SetPositionAndRotation(firePosition.position, firePosition.rotation * Quaternion.Euler(90, 0, 90));
-                newMuzzle.gameObject.SetActive(true);
-                Destroy(newMuzzle, 0.12f);
-
-                // ÃÑ¾Ë »ý¼ºÇÏ±â
-                var newBullet = Instantiate(bulletPrefab);
-                var cameraTransform = Camera.main.transform;
-                newBullet.transform.SetPositionAndRotation(cameraTransform.position, cameraTransform.rotation);
-                newBullet.gameObject.SetActive(true);
-
-                // ÅºÇÇ »ý¼º
-
-                var newbulletCartridge = Instantiate(bulletCartridgePrefab);
-                newbulletCartridge.transform.SetPositionAndRotation(bulletCartRidgePosition.position, bulletCartRidgePosition.rotation);
-                newbulletCartridge.GetComponent<Rigidbody>().AddForce(Vector3.left);
-
-                Vector3 velocity = recoilShakePattern[currentRecoilIndex];
-                currentRecoilIndex++;
-                if (currentRecoilIndex >= recoilShakePattern.Count)
-                {
-                    currentRecoilIndex = currentRecoilIndex = 0;
-                }
-                CameraSystem.Instance.ShakeCamera(velocity, 0.2f, 1f);
-
-            }
+            
         }
     }
 }
